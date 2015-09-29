a794fd290e49026244d5c416107b5349ee2b7a5e
Aaron Cody <aaron.cody@gmail.com>
2015-09-29 10:58:57 -0700

    Added Admin 2 Bootstrap theme and example pages.

--- build.js
+++ build.js
@@ -1,27 +1,43 @@
-var concat = require('./plugins/concat.js')
-var MetalsmithLayouts = require('metalsmith-layouts');
-var MetalsmithInPlace = require('metalsmith-in-place');
-var Metalsmith = require('metalsmith');
+var concat = require('./plugins/concat.js'),
+    congregate = require('./plugins/congregate.js'),
+    assets = require('metalsmith-assets'),
+    MetalsmithLayouts = require('metalsmith-layouts'),
+    MetalsmithInPlace = require('metalsmith-in-place'),
+    Metalsmith = require('metalsmith');
 
 /**
  * Build.
  */
 
 var metalsmith = Metalsmith(__dirname)
+  .source("./assets")
   .metadata({
     "title": "Camouflage Test App",
     "description": "A testing platform at the intersection of JS snippets and JS frameworks"
   })
   .use(concat)
+  .use(congregate({
+    "files": ["./node_modules/jquery/dist/jquery.js",
+              "./node_modules/bootstrap/dist/js/bootstrap.js",
+              "./node_modules/metismenu/dist/metisMenu.js"],
+    "output": "./scripts/"
+  }))
+  .use(congregate({
+    "files": ["./node_modules/font-awesome/css/font-awesome.css"],
+    "output": "./styles/"
+  }))
+  .use(assets({
+    "source": "./node_modules/font-awesome/fonts/",
+    "destination": "./fonts/"
+  }))
   .use(MetalsmithLayouts({
     "engine": "handlebars",
-    "directory": "./src/layouts/"
+    "directory": "./assets/layouts/"
   }))
   .use(MetalsmithInPlace({
     "engine": "handlebars",
-    "partials": "./src/views/"
+    "partials": "./assets/partials/"
   }))
   .build(function(err){
     if (err) throw err;
   });
-
