3677ba638c105ae43eb3cfd24b9ff8fbae5cfa01
Aaron Cody <aaron.cody@gmail.com>
2015-10-21 00:47:50 -0700

    Added basic ajax-driven turbolinks simulator

---
+++ assets/scripts/camouflage-scripts.js
@@ -0,0 +1,74 @@
+var getFormData = function($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+      indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+  },
+  useridentification = {
+    init: function(){
+      var store = {};
+      if(store = localStorage.getItem("camouflageuser")){
+        if(JSON.parse(store).loggedin) this.populate(JSON.parse(store));
+      }
+    },
+    save: function(loggedin){
+      var formData = getFormData($("#useridentification"));
+      formData.loggedin = loggedin;
+      localStorage.setItem("camouflageuser",  JSON.stringify(formData));
+    },
+    populate: function(localstorageobject){
+      for(var index in localstorageobject) {
+        if (localstorageobject.hasOwnProperty(index)) {
+          var stored = localstorageobject[index];
+          $('[name=' + index + ']').val(stored);
+          $("." + index + '-text').text(stored);
+        }
+      }
+    },
+    generateRandomNumber: function(){
+      $('[name=randomnumber]').val(Math.floor(Math.random() * 10000));
+      return false;
+    }
+  };
+
+  turbolinks = {
+    localStorageName: 'camouflageturbolinks',
+    init: function(){
+      if(this.turbolinksEnabled()) {
+        $('.turbolinks-enabled').attr('checked','');
+      }
+      $('.turbolinks-enabled').on('click', function(event){
+        this.enableTurbolinks(event.target.checked);
+        this.setupEvents();
+        if(!this.turbolinksEnabled()) window.location.reload();
+      }.bind(this));
+      this.setupEvents();
+    },
+    setupEvents: function(){
+      if(this.turbolinksEnabled()){
+        $('a:not([href="#"])').on('click', this.linkHandler.bind(this));
+      } else {
+        $('a:not([href="#"])').off('click', this.linkHandler.bind(this));
+      }
+    },
+    enableTurbolinks: function(on) {
+      localStorage.setItem(this.localStorageName,  on);
+    },
+    turbolinksEnabled: function() {
+      return localStorage.getItem(this.localStorageName) === "true";
+    },
+    linkHandler: function(event){
+      event.preventDefault();
+      var address = event.target.getAttribute('href');
+      $("body").load(address, function(){
+        $(window).trigger('resize');
+      });
+    }
+  };
+
+useridentification.init();
+turbolinks.init();
